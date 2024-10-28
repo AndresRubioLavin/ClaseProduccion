9d153c5bca6c96433702b7d6f383c8aab50aeae5
AndresRubioLavin <162493063+AndresRubioLavin@users.noreply.github.com>
2024-10-27 18:45:00 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+function cambiarPantalla1() {
+    var Pag1 = document.getElementById("Pag1");
+    var Pag2 = document.getElementById("Pag2");
+    var Pag3 = document.getElementById("Pag3");
+    var Pag4 = document.getElementById("Pag4");
+    var Pag5 = document.getElementById("Pag5");
+    Pag1.style.display = "flex";
+    Pag2.style.display = "none";
+    Pag3.style.display = "none";
+    Pag4.style.display = "none";
+    Pag5.style.display = "none";
+}
+
+function cambiarPantalla2() {
+    var Pag1 = document.getElementById("Pag1");
+    var Pag2 = document.getElementById("Pag2");
+    var Pag3 = document.getElementById("Pag3");
+    var Pag4 = document.getElementById("Pag4");
+    var Pag5 = document.getElementById("Pag5");
+    Pag2.style.display = "flex";
+    Pag1.style.display = "none";
+    Pag3.style.display = "none";
+    Pag4.style.display = "none";
+    Pag5.style.display = "none";
+}
+
+function cambiarPantalla3() {
+    var Pag1 = document.getElementById("Pag1");
+    var Pag2 = document.getElementById("Pag2");
+    var Pag3 = document.getElementById("Pag3");
+    var Pag4 = document.getElementById("Pag4");
+    var Pag5 = document.getElementById("Pag5");
+    Pag3.style.display = "flex";
+    Pag1.style.display = "none";
+    Pag2.style.display = "none";
+    Pag4.style.display = "none";
+    Pag5.style.display = "none";
+}
+
+function cambiarPantalla4() {
+    var Pag1 = document.getElementById("Pag1");
+    var Pag2 = document.getElementById("Pag2");
+    var Pag3 = document.getElementById("Pag3");
+    var Pag4 = document.getElementById("Pag4");
+    var Pag5 = document.getElementById("Pag5");
+    Pag4.style.display = "flex";
+    Pag1.style.display = "none";
+    Pag2.style.display = "none";
+    Pag3.style.display = "none";
+    Pag5.style.display = "none";
+}
